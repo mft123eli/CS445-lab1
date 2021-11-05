6521aea4b86139c884476da5b6657fcbb39ae117
mft123eli <89947358+mft123eli@users.noreply.github.com>
2021-11-04 19:02:18 -0500

    lab5 exercise

--- lab5/exercise1.js
+++ lab5/exercise1.js
@@ -2,14 +2,15 @@ const isPrime = num => new Promise(function(resolve, reject) {
     setTimeout(function (){ 
         
         for(let i = 2, s = Math.sqrt(num); i <= s; i++)
-                if(num % i !== 0) 
-                resolve ({prime: true})  
+                if(num % i === 0) 
+                reject ({prime: false})     
                 else
-                reject ({prime: false})   
+                resolve ({prime: true})  
+                
     }, 500);
 });
 console.log ('start');
-isPrime(11)
+isPrime(4)
     .then(message=> console.log(message))
     .catch(error=> console.log(error))
 console.log ('end');
